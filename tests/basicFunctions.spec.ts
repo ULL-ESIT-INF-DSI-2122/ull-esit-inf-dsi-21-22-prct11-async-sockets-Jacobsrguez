e682b9d2cff6aa550becc24ce822b8fd05898abf
Jacob Santana <alu0101330426@ull.edu.es>
2022-05-06 23:26:02 +0000

    Copia archivos de la p9

--- tests/basicFunctions.spec.ts
+++ tests/basicFunctions.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import {expect} from 'chai';
-import {add, substract} from '../src/basicFunctions';
+import {add, substract} from '../src/pruebas/basicFunctions';
 
 describe('add function tests', () => {
   it('add(1, 8) returns value 9', () => {
