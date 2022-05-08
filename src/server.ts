01ac749aae3ba9b5084f0dcc66683ce41ee1b9f6
jacob santana rodriguez <72451634+Jacobsrguez@users.noreply.github.com>
2022-05-08 01:16:11 +0100

    modificaciones en la clase server

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,27 @@ import {Note} from './nota';
 import {MessageEventEmitterServer} from './eventEmitterServer';
 import * as net from 'net';
 import {Method} from './method';
+import {ResponseType} from './type/response';
 
 net.createServer((connection) => {
   const server = new MessageEventEmitterServer(connection);
   server.on('message', (message) => {
     switch (message.action) {
       case 'add':
-        connection.write(JSON.stringify({
-        action: 'add',
-        
-        }));
+        let result: Boolean = False;
+        const nota = new Note(message.title, messsage.body. message.color);
+        new method().add(nota, message.user, (data, _) => {
+          if (data) {
+            result = True;
+          }
+        });
+        const resquest: ResponseType = {
+          type: 'add',
+          succes: result
+        }
+        connection.write(JSON.stringify(resquest))
         break;
       default:
-        console.log(chalk.red('Accion no reconocida'));
         break;
     }
   });
